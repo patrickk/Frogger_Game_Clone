f2a9d5654321538382546f26b6076a79c93a54d5
patrickk93 <patrickkim93@gmai.com>
2016-11-08 15:58:49 +0900

    Add files for arcade game first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Frogger Game Remake
+
+This project uses the OOP approach to recreate the classic Frogger
+Game. **Class functions** and **Class prototype functions** were used to create player and enemy properties and methods.
+
+## How To Run Game
+
+In order to run the game, start up your browser with the HTML file in the folder. **Make sure all folders and files are left in place.** Once the HTML file has been loaded in the browser, a gameboard will appear on the center of the screen with a character at the bottom and enemies (bugs) in the middle.
+
+##How To Play Game
+
+In order to win, the user must navigate the player to the end of the board, where the water is. If the player gets hit by the bugs, player is sent back to the start.
+
+## TODO
+
+- Scoreboard with score and lives
+- Make gameboard wider
+- Add gems to be collected by player, which increments score
+- Add inanimate objects such as rocks/branches on board
+- After every 100 or so points, reward one life
+- Allow user to pick different characters other than `char-boy`
+- Different levels with each being more difficult than one before
+
+
+## Credits
+
+- Udacity for the game engine and sprites
+- MDN for countless documentation
---
+++ js/app.js
@@ -0,0 +1,103 @@
+var maxSpeed = 350;
+var minSpeed = 100;
+
+//Class function for enemies
+var Enemy = function(x,y,speed) {
+  this.sprite = 'images/enemy-bug.png';
+  this.x = x;
+  /*Y should be between 50 and 230... 50 1st row, 140 2nd, 230 3rd*/
+  this.y = y;
+  this.speed = Math.floor(Math.random()*(maxSpeed - minSpeed + 1))+minSpeed;
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+  this.x = this.x + (this.speed * dt);
+  if (this.x >= 410) {
+    this.x = -10;
+  }
+  this.checkCollisions();
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+//Check for collisions between bugs and player
+//Using the Axis-Aligned Bounding Box algorithm from MDN
+Enemy.prototype.checkCollisions = function() {
+  allEnemies.forEach(function(box){
+    if (box.x < player.x + 50 &&
+        box.x + 50 > player.x &&
+        box.y < player.y + 50 &&
+        box.y+ 50 > player.y) {
+          player.resetPosition();
+          document.getElementById("message").innerHTML = "Get wrecked";
+          // console.log("Get wrecked");
+        }
+  });
+};
+
+//Class function for player
+var Player = function () {
+  this.sprite = "images/char-boy.png"
+  this.x = 200;
+  this.y = 400;
+  /*Player cannot go above y: 400px, x: 410px
+    Once player hits y: 0 px, reset game and add to score*/
+};
+
+//Updates player position on game
+Player.prototype.update = function() {
+  //Left this blank and the game still works.. ??
+};
+
+//Draws player sprite onto game
+Player.prototype.render = function () {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+//Handles input from user, logic ensures player cannot move off screen
+//If player gets to end of map, reset game
+Player.prototype.handleInput = function (allowedKeys) {
+  if (allowedKeys === 'left' && this.x > 0){
+    this.x = this.x-50;
+  }
+  if (allowedKeys === 'right' && this.x <= 399) {
+    this.x = this.x+50;
+  }
+  if (allowedKeys === 'down' && this.y < 400) {
+    this.y = this.y+50;
+  }
+  if (allowedKeys === 'up' && this.y > 0) {
+    this.y = this.y-50;
+  }
+  if (this.y <= 0) {
+    document.querySelector("#message").innerHTML = "GOOD JOB!!!!!";
+    this.resetPosition();
+  }
+};
+
+//Player resets to said coordinates if collision or win
+Player.prototype.resetPosition = function() {
+    this.x = 200;
+    this.y = 400;
+};
+
+//Instantiate enemy objects and player object
+var allEnemies = [new Enemy(50,50), new Enemy(50,140), new Enemy(50,230), new Enemy(50, 50)];    //[enemy1, enemy2, enemy3];
+var player = new Player();
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+    player.handleInput(allowedKeys[e.keyCode]);
+});
